4fa138eae15bfbab38a62299a65fffed388b742b
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-18 10:22:59 -0500

    Create user login request spec

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     # create_user = JSON.parse(request.raw_post, symbolize_names: true)
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       render json: UserSerializer.new(user), status: 201
     else
       render json: {errors: {details: "There was an error completing this request."}}, status: 400
