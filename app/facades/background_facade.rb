4fe12e2719469edec9764ba2daab9d2cf1effd78
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-17 21:44:10 -0500

    Create background spec

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -3,7 +3,6 @@ class BackgroundFacade
     def image(location)
       data = BackgroundService.get_background(location)
       Background.new(data)
-      # data[:photos].first[:locations].first[:latLng]
     end
   end
 end
--- spec/poros/background_spec.rb
+++ spec/poros/background_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Background', :vcr do
   it 'returns an image based on search params' do
     image = BackgroundFacade.image('hoboken,nj')
+
+    expect(image.image).to be_a(Hash)
+    expect(image.image).to have_key(:image_url)
+    expect(image.image).to have_key(:credit)
+    expect(image.image[:credit]).to have_key(:source)
+    expect(image.image[:credit]).to have_key(:photographer)
+    expect(image.image[:credit]).to have_key(:photographer_url)
+    expect(image.image[:credit]).to_not have_key(:width)
+    expect(image.image[:credit]).to_not have_key(:height)
   end
 end
