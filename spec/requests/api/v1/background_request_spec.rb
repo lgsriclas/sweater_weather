71b320d002467f3d91ccf07cead8957c52666781
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-17 21:39:33 -0500

    Create background request spec

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe 'Pexels API', :vcr do
 
     expect(response).to be_successful
     expect(response.status).to eq 200
+
+    background = JSON.parse(response.body, symbolize_names: true)
+
+    expect(background).to be_a(Hash)
+    expect(background).to have_key(:data)
+    expect(background[:data]).to have_key(:attributes)
+    expect(background[:data][:attributes]).to have_key(:image)
+    expect(background[:data][:attributes][:image]).to have_key(:image_url)
+    expect(background[:data][:attributes][:image]).to have_key(:credit)
+    expect(background[:data][:attributes][:image][:credit]).to have_key(:source)
+    expect(background[:data][:attributes][:image][:credit]).to have_key(:photographer)
+    expect(background[:data][:attributes][:image][:credit]).to have_key(:photographer_url)
+    expect(background[:data][:attributes][:image][:credit]).to_not have_key(:width)
+    expect(background[:data][:attributes][:image][:credit]).to_not have_key(:height)
   end
 end
