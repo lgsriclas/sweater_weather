b552a9b39cfc9bacc594e532d8fe2afd71af92ff
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-18 18:12:03 -0500

    Update controller for sad path

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,24 +1,15 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # time = MapService.get_travel_time(params[:origin], params[:destination])
-    # travel_time = time[:formattedTime]
-    # lat = MapService.get_location(params[:destination])[:latLng][:lat]
-    # lon = MapService.get_location(params[:destination])[:latLng][:lat]
-    # hourly_weather = WeatherService.get_weather_forecast(lat, lon)
-    # road_trip =
-    #   {
-    #     origin: params[:origin],
-    #     destination: params[:destination],
-    #     travel_time: travel_time,
-    #     weather_at_eta: {
-    #       temperature: hourly_weather[:temp],
-    #       conditions: hourly_weather[:weather][0][:description]}
-    #   }
-    # RoadTrip.new(road_trip)
-    road_trip = RoadTripFacade.road_trip(params[:origin], params[:destination])
+    if params[:api_key] == ""
+      render json: {errors: {details: "Your API key is incorrect"}}, status: 401
+    else
+      road_trip = RoadTripFacade.road_trip(params[:origin], params[:destination])
 
-    render json: RoadTripSerializer.new(road_trip)
+      render json: RoadTripSerializer.new(road_trip)
+    end 
   end
+
+  private
 
   def road_trip_params
     params.permit(:origin, :destination, :api_key)
--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,8 +1,6 @@
 class WeatherFacade
   class << self
     def weather_forecast(lat, lon)
-      # location = MapFacade.coordinates(params[:location])
-      # city = MapFacade.coordinates(location)
       forecast = WeatherService.get_weather_forecast(lat, lon)
       Forecast.new(forecast)
     end
