817357545228c5718ca37b5b2e65f29603eb1fd3
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-19 12:29:42 -0500

    set up error handling for quantity

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BreweriesController < ApplicationController
   def index
-    breweries = BreweryFacade.breweries(params[:location], params[:quantity])
-    render json: BrewerySerializer.new(breweries)
+    if params[:quantity].to_i > 0
+      breweries = BreweryFacade.breweries(params[:location], params[:quantity])
+      render json: BrewerySerializer.new(breweries)
+    else
+      render json: {errors: {details: "Please enter a quantity greater than 0."}}, status: 400
+    end 
   end
 end
--- spec/requests/api/v1/breweries_request_spec.rb
+++ spec/requests/api/v1/breweries_request_spec.rb
@@ -14,5 +14,21 @@ RSpec.describe 'Breweries Request', :vcr do
     breweries = JSON.parse(response.body, symbolize_names: true)
 
     expect(breweries).to be_a(Hash)
+    expect(breweries).to have_key(:data)
+    expect(breweries[:data]).to have_key(:attributes)
+    expect(breweries[:data][:attributes]).to have_key(:destination)
+    expect(breweries[:data][:attributes]).to have_key(:forecast)
+    expect(breweries[:data][:attributes]).to have_key(:breweries)
+  end
+
+  it 'sends an error code if quantity is 0 or less' do
+    get '/api/v1/breweries?location=denver,co&quantity=-1',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
   end
 end
